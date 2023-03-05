8bb5df6b8b7bfa5ba7761f1ecbf95a7fa3b8c959
tars3017 <prgmpclh@gmail.com>
2023-03-05 20:15:33 +0800

    Finish almost everything in src file. Bugs not been fixed

--- src/control.cpp
+++ src/control.cpp
@@ -3,11 +3,47 @@
 #include "std_msgs/Float64.h"
 #include "std_msgs/Bool.h"
 
-void dist_cb(const mecanum_stead::location::ConstPtr& msg) {
+#define ERROR_TOR 0.05
+#define p_VALUE 0.3
+#define q_VALUE 0.3
+#define MAX_SPEED 70
 
+double start_x, start_y, start_z;
+double target_x, target_y, target_z;
+double now_x, now_y, now_z;
+void dist_cb(const mecanum_stead::location::ConstPtr& msg) {
+    start_x = msg->x;
+    start_y = msg->y;
+    start_z = msg->z;
+    target_x = msg->target_x;
+    target_y = msg->target_y;
+    target_z = msg->target_z;
+}
+bool error() {
+    bool able_to_stop = true;
+    able_to_stop &= (abs(target_x - now_x) < ERROR_TOR);
+    able_to_stop &= (abs(target_y - now_y) < ERROR_TOR);
+    able_to_stop &= (abs(target_z - now_z) < ERROR_TOR);
+    return able_to_stop;
 }
 void state_cb(const mecanum_stead::location::ConstPtr& msg) {
+    now_x = msg->x;
+    now_y = msg->y;
+    now_z = msg->z;
+}
+double get_vel(double start, double now, double target) {
+    double ret_vel;
+    if (abs(now-start)*p_VALUE < abs(target-now)*q_VALUE) {
+        ret_vel = (now - start) * p_VALUE;
+    }
+    else {
+        ret_vel = (target - now) * q_VALUE;
+    }
 
+    if (abs(ret_vel) > MAX_SPEED) {
+        ret_vel = (ret_vel > 0 ? MAX_SPEED : -MAX_SPEED);
+    }
+    return ret_vel;
 }
 int main(int argc, char** argv) {
     ros::init(argc, argv, "control");
@@ -16,10 +52,29 @@ int main(int argc, char** argv) {
     ros::Subscriber sub_dist = nh.subscribe("/setpoint", 10, dist_cb);
     ros::Subscriber sub_state = nh.subscribe("/state", 10, state_cb);
     ros::Publisher pub_next_ctl = nh.advertise<std_msgs::Bool>("/next_ctl", 10);
-    ros::Publisher pub_control = nh.advertise<std_msgs::Float64>("/control_effort", 10);
+    ros::Publisher pub_control = nh.advertise<mecanum_steady::location>("/control_effort", 10);
 
+
+    std_msgs::Bool go_next;
+    mecanum::steady::location vel; 
+
+    go_next.data = 0;
     while (ros::ok()) {
-        // TODO
+        ros::spinOnce();
+        if (in_error()) {
+            go_next.data = 1;
+            vel.x = 0;
+            vel.y = 0;
+            vel.z = 0;
+        }
+        else {
+            go_next.data = 0;
+            vel.x = get_vel(start_x, now_x, target_x);
+            vel.y = get_vel(start_y, now_y, target_y);
+            vel.z = get_vel(start_z, now_z, target_z);
+        }
+        pub_next_ctl.publish(go_next);
+        pub_control.publish(vel);
     }
     return 0;
 }
--- src/drive.cpp
+++ src/drive.cpp
@@ -3,8 +3,17 @@
 #include "std_msgs/Float64.h"
 #include "std_msgs/Bool.h"
 
-void control_cb(const std_msgs::Float64::ConstPtr& msg) {
+double last_x_vel, last_y_vel, last_z_vel;
+double cur_x_vel, cur_y_vel, cur_z_vel;
 
+double x_pos, y_pos, z_pos;
+void control_cb(const std_msgs::Float64::ConstPtr& msg) {
+    cur_x_vel = msg->x;
+    cur_y_vel = msg->y;
+    cur_z_vel = msg->z;
+}
+void encoder_cb(const mecanum_steady::location::ConstPtr& msg) {
+    // TODO
 }
 int main(int argc, char** argv) {
     ros::init(argc, argv, "drive");
@@ -12,9 +21,38 @@ int main(int argc, char** argv) {
 
     ros::Subscriber sub_control = nh.subscribe("/control_effort", 10, control_cb);
     ros::Publisher pub_state = nh.advertise<mecanum_steady::location>("/state", 10);
+    
+    // below 2 lines are for temporary usage
+    ros::Subscriber sub_encoder = nh.subscribe("/encoder", 10, encoder_cb);
+    ros::Publisher pub_motor = nh.advertise<mecanum_steady::location>("/motor_speed", 10);
 
+    last_x_vel = 0, last_y_vel = 0, last_z_vel = 0;
+    cur_x_vel = 0, cur_y_vel = 0, cur_z_vel = 0;
+    x_pos = 0, y_pos = 0, z_pos = 0;
+    ros::Time last_time, now_time;
+
+    last_time = ros::Time::now();
+    mecanum_steady::locatino output_vel;
     while (ros::ok()) {
-        // TODO
+        ros::spinOnce();
+
+        /* temporary for Rviz */
+        last_x_vel = cur_x_vel;
+        last_y_vel = cur_y_vel;
+        last_z_vel = cur_z_vel;
+        /* temporary for Rviz */
+
+        now_time = ros::Time::now();
+        x_pos += (last_x_vel) * (now_time - last_time); 
+        y_pos += (last_y_vel) * (now_time - last_time); 
+        z_pos += (last_z_vel) * (now_time - last_time); 
+
+        
+        output_vel.x = cur_x_vel;
+        output_vel.y = cur_y_vel;
+        output_vel.z = cur_z_vel;
+        pub_motor.publish(output_vel);
+        last_time = ros::Time::now();
     }
     return 0;
 }
