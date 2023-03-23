dddc1a098bf9ba310d5fd1f5c51f7a795858f296
tars3017 <prgmpclh@gmail.com>
2023-03-23 18:55:19 +0800

    Change the algorithm of vel output

--- src/control.cpp
+++ src/control.cpp
@@ -4,15 +4,32 @@
 #include "std_msgs/Bool.h"
 #include <algorithm>
 
-#define ERROR_TOR 0.032
-#define p_VALUE 1.7
-#define q_VALUE 1.7
-#define MAX_SPEED 5.0
-#define START_CONST 0.8
 
 double start_x, start_y, start_z;
 double target_x, target_y, target_z;
 double now_x, now_y, now_z;
+double total_x, total_y, total_z;
+
+// param
+double x_tol;
+double y_tol;
+double z_tol;
+
+double acc_xy;
+double acc_zz;
+
+double acc_x_frac;
+double acc_y_frac;
+double acc_z_frac;
+
+double max_xy_vel;
+double min_xy_vel;
+double max_zz_vel;
+
+double kp_xy;
+double kp_zz;
+// param
+double x_err, y_err, z_err;
 void dist_cb(const mecanum_steady::location::ConstPtr& msg) {
     // start_x = msg->start_x;
     // start_y = msg->start_y;
@@ -20,13 +37,21 @@ void dist_cb(const mecanum_steady::location::ConstPtr& msg) {
     target_x = msg->x;
     target_y = msg->y;
     target_z = msg->z;
+
+    total_x = target_x - now_x;
+    total_y = target_y - now_y;
+    total_z = target_z - now_z;
+    /* ROS_INFO("target %lf total %lf", target_x, total_x); */
 }
 bool in_error() {
-    if (target_x-now_x == 0 && target_y-now_y == 0 && target_z-now_z == 0) return 0;
+    if (x_err == 0 && y_err == 0 && z_err == 0) return 0;
     bool able_to_stop = true;
-    able_to_stop &= (abs(target_x - now_x) < ERROR_TOR);
-    able_to_stop &= (abs(target_y - now_y) < ERROR_TOR);
-    able_to_stop &= (abs(target_z - now_z) < ERROR_TOR);
+    able_to_stop &= (fabs(x_err) < x_tol);
+    able_to_stop &= (fabs(y_err) < y_tol);
+    able_to_stop &= (fabs(z_err) < z_tol);
+    /* ROS_INFO("err1 %lf %lf", fabs(x_err), x_tol); */
+    /* ROS_INFO("err2 %lf %lf", fabs(y_err), y_tol); */
+    /* ROS_INFO("err3 %lf %lf", fabs(z_err), z_tol); */
     if (able_to_stop) {
         start_x = now_x;
         start_y = now_y;
@@ -39,15 +64,6 @@ void state_cb(const mecanum_steady::location::ConstPtr& msg) {
     now_y = msg->y;
     now_z = msg->z;
 }
-double get_vel(double start, double now, double target) {
-    double ret_vel;
-    double p = abs(now-start)*p_VALUE + START_CONST;
-    double q = abs(target-now)*q_VALUE;
-    ret_vel = std::min(p, q);
-    ret_vel = std::min(ret_vel, MAX_SPEED);
-    if (target - now < 0) ret_vel *= (-1);
-    return ret_vel;
-}
 int main(int argc, char** argv) {
     ros::init(argc, argv, "control");
     ros::NodeHandle nh;
@@ -57,30 +73,89 @@ int main(int argc, char** argv) {
     ros::Publisher pub_next_ctl = nh.advertise<std_msgs::Bool>("/next_ctl", 10);
     ros::Publisher pub_control = nh.advertise<mecanum_steady::location>("/control_effort", 10);
 
+    nh.getParam("x_tol", x_tol);
+    nh.getParam("y_tol", y_tol);
+    nh.getParam("z_tol", z_tol);
+    nh.getParam("acc_xy", acc_xy);
+    nh.getParam("acc_zz", acc_zz);
+    nh.getParam("acc_x_frac", acc_x_frac);
+    nh.getParam("acc_y_frac", acc_y_frac);
+
+    nh.getParam("acc_z_frac", acc_z_frac);
+    nh.getParam("max_xy_vel", max_xy_vel);
+    nh.getParam("min_xy_vel", min_xy_vel);
+    nh.getParam("max_zz_vel", max_zz_vel);
+    nh.getParam("kp_xy", kp_xy);
+    nh.getParam("kp_zz", kp_zz);
+
 
     std_msgs::Bool go_next;
     mecanum_steady::location vel; 
+    double pub_x, pub_y, pub_z;
+    double vel_x, vel_y, vel_z;
 
     go_next.data = 0;
     while (ros::ok()) {
         ros::spinOnce();
+
+        x_err = target_x - now_x;
+        y_err = target_y - now_y;
+        z_err = target_z - now_z;
+
         if (in_error()) {
-            /* ROS_INFO("in error"); */
+            ROS_INFO("in error");
             go_next.data = 1;
-            vel.x = 0;
-            vel.y = 0;
-            vel.z = 0;
-            ROS_INFO("now_x %lf", now_x);
+            vel_x = 0, vel_y = 0, vel_z = 0;
+            pub_x = 0, pub_y = 0, pub_z = 0;
+            ROS_INFO("now_x %lf %lf %lf", start_x, now_x, target_x);
         }
         else {
             go_next.data = 0;
-            /* ROS_INFO("control %lf %lf %lf", start_x, now_x, target_x); */
-            /* ROS_INFO("expect %lf, %lf", (now_x-start_x)*p_VALUE, (target_x-now_x)*q_VALUE); */
-            vel.x = get_vel(start_x, now_x, target_x);
-            /* ROS_INFO("vel %lf", vel.x); */
-            vel.y = get_vel(start_y, now_y, target_y);
-            vel.z = get_vel(start_z, now_z, target_z);
+            if (fabs(x_err) > x_tol && fabs(x_err) < fabs(total_x*acc_x_frac)) {
+                vel_x += (x_err > 0 ? acc_xy : -acc_xy);
+                if (vel_x > max_xy_vel) vel_x = max_xy_vel;
+                else if (vel_x < -max_xy_vel) vel_x = -max_xy_vel;
+                pub_x = vel_x;            
+            }
+            else if (fabs(x_err) > x_tol) {
+                vel_x = kp_xy * x_err;
+                if (vel_x > max_xy_vel) vel_x = max_xy_vel;
+                else if (vel_x < -max_xy_vel) vel_x = -max_xy_vel;
+                else if (vel_x > 0 && vel_x < min_xy_vel) vel_x = min_xy_vel;
+                else if (vel_x < 0 && vel_x > -min_xy_vel) vel_x = -min_xy_vel;
+                pub_x = vel_x;
+            }
+
+            if (fabs(y_err) > y_tol && fabs(y_err) < fabs(total_y*acc_y_frac)) {
+                vel_y += (y_err > 0 ? acc_xy : -acc_xy);
+                if (vel_y > max_xy_vel) vel_y = max_xy_vel;
+                else if (vel_y < -max_xy_vel) vel_y = -max_xy_vel;
+                pub_y = vel_y;            
+            }
+            else if (fabs(y_err) > y_tol) {
+                vel_y = kp_xy * y_err;
+                if (vel_y > max_xy_vel) vel_y = max_xy_vel;
+                else if (vel_y < -max_xy_vel) vel_y = -max_xy_vel;
+                else if (vel_y > 0 && vel_y < min_xy_vel) vel_y = min_xy_vel;
+                else if (vel_y < 0 && vel_y > -min_xy_vel) vel_y = -min_xy_vel;
+                pub_y = vel_y;
+            }
+            ROS_INFO("vel_y %lf", vel_y);
+
+            if (fabs(z_err) > z_tol && fabs(z_err) < fabs(total_z*acc_z_frac)) {
+                vel_z += (z_err > 0 ? acc_zz : -acc_zz);
+                if (vel_z > max_zz_vel) vel_z = max_zz_vel;
+                else if (vel_z < -max_zz_vel) vel_z = -max_zz_vel;
+                pub_z = vel_z;            
+            }
+            else if (fabs(z_err) > z_tol) {
+                vel_z = kp_zz * z_err;
+                if (vel_z > max_zz_vel) vel_z = max_zz_vel;
+                else if (vel_z < -max_zz_vel) vel_z = -max_zz_vel;
+                pub_z = vel_z;
+            }
         }
+        vel.x = pub_x, vel.y = pub_y, vel.z = pub_z; 
         pub_next_ctl.publish(go_next);
         pub_control.publish(vel);
     }
