76ad635af2cdcf25a1d197a5d4bc442e98d4c63d
tars3017 <prgmpclh@gmail.com>
2023-03-29 23:08:10 +0800

    Add stm_communication.cpp

---
+++ src/stm_communication.cpp
@@ -0,0 +1,25 @@
+/** stm_communication.cpp **/
+#include <iostream>
+#include "ros/ros.h"
+#include <std_msgs/Int64.h>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "stm_communication");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub = nh.advertise<std_msgs::Int64>("counting", 1);
+
+    std_msgs::Int64 hello;
+    hello.data = 0;
+
+    while(ros::ok()){
+        pub.publish(hello);
+        hello.data++;
+        ros::Duration(0.5).sleep();
+    }
+
+    ROS_INFO("== Terminate ! ==");
+    return 0;
+}
